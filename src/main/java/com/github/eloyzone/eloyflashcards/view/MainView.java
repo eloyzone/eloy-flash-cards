976cb36016e593686a4a3777a71fa93c32e9a171
Elyas Hadizadeh Tasbiti <elyas_hadizadeh@hotmail.com>
2019-07-09 22:21:21 +0430

    add a top controller to main view

--- src/main/java/com/github/eloyzone/eloyflashcards/view/MainView.java
+++ src/main/java/com/github/eloyzone/eloyflashcards/view/MainView.java
@@ -1,9 +1,14 @@
 package com.github.eloyzone.eloyflashcards.view;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainView extends Application
@@ -13,7 +18,12 @@ public class MainView extends Application
     {
         BorderPane mainBorderPane = new BorderPane();
 
+        HBox topControllerHBox = createTopController();
+        VBox centerVBox = new VBox();
+        centerVBox.getChildren().addAll(topControllerHBox);
+
         mainBorderPane.setTop(new MenuBar(this));
+        mainBorderPane.setCenter(centerVBox);
 
         Scene scene = new Scene(mainBorderPane, 700, 700);
         primaryStage.setScene(scene);
@@ -21,5 +31,22 @@ public class MainView extends Application
         primaryStage.getIcons().addAll(new Image(getClass().getClassLoader().getResourceAsStream("images/icon_eloy_flash_card_mini.png")));
         primaryStage.setTitle("Eloy Flash Cards");
         primaryStage.show();
+    }
+
+    private HBox createTopController()
+    {
+        HBox hBox = new HBox();
+
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getStyleClass().add("node-border");
+        hBox.setPadding(new Insets(5, 0, 5, 0));
+
+        Button showDecksButton = new Button("Decks");
+        Button addDeckButton = new Button("Add Deck");
+        Button addCardButton = new Button("Add Card");
+
+        hBox.getChildren().addAll(showDecksButton, addDeckButton, addCardButton);
+
+        return hBox;
     }
 }
