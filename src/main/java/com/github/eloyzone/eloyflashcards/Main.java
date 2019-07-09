26c266ab605b7e847261de19b07bc7f484213df2
Elyas Hadizadeh Tasbiti <elyas_hadizadeh@hotmail.com>
2019-07-09 21:41:39 +0430

    The essential steps for developing a java app

---
+++ src/main/java/com/github/eloyzone/eloyflashcards/Main.java
@@ -0,0 +1,12 @@
+package com.github.eloyzone.eloyflashcards;
+
+import com.github.eloyzone.eloyflashcards.view.MainView;
+import javafx.application.Application;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Application.launch(MainView.class, args);
+    }
+}
---
+++ src/main/java/com/github/eloyzone/eloyflashcards/view/MainView.java
@@ -0,0 +1,22 @@
+package com.github.eloyzone.eloyflashcards.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        BorderPane mainBorderPane = new BorderPane();
+
+        Scene scene = new Scene(mainBorderPane, 700, 700);
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle("Eloy Flash Cards");
+        primaryStage.show();
+    }
+}
